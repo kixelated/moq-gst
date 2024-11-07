12fc63e1852538b43f82ae3ee26eb0ea29728f3e
Luke Curley <kixelated@gmail.com>
2024-11-06 20:03:17 -0800

    WIP

--- src/sink/imp.rs
+++ src/sink/imp.rs
@@ -1,11 +1,14 @@
 use anyhow::Context as _;
+use bytes::Bytes;
 use gst::glib;
 use gst::prelude::*;
 use gst::subclass::prelude::*;
-use gst_base::subclass::prelude::*;
+use gst::Pad;
 
+use moq_karp::{catalog, moq_transfork};
 use moq_native::{quic, tls};
 use once_cell::sync::Lazy;
+use std::collections::HashMap;
 use std::sync::Arc;
 use std::sync::Mutex;
 
@@ -26,9 +29,9 @@ struct Settings {
 
 #[derive(Default)]
 struct State {
-    pub media: Option<moq_karp::cmaf::Import>,
+    pub broadcast: Option<moq_karp::produce::Broadcast>,
+    pub tracks: HashMap<String, moq_karp::produce::Track>,
     pub session: Option<moq_transfork::Session>,
-    pub published: bool, // true if the media has been published
 }
 
 #[derive(Default)]
@@ -41,12 +44,14 @@ pub struct MoqSink {
 impl ObjectSubclass for MoqSink {
     const NAME: &'static str = "MoqSink";
     type Type = super::MoqSink;
-    type ParentType = gst_base::BaseSink;
+    type ParentType = gst::Element;
 
     fn new() -> Self {
         Self::default()
     }
 }
+
+impl GstObjectImpl for MoqSink {}
 
 impl ObjectImpl for MoqSink {
     fn properties() -> &'static [glib::ParamSpec] {
@@ -94,8 +99,7 @@ impl ObjectImpl for MoqSink {
     }
 }
 
-impl GstObjectImpl for MoqSink {}
-
+impl BinImpl for MoqSink {}
 impl ElementImpl for MoqSink {
     fn metadata() -> Option<&'static gst::subclass::ElementMetadata> {
         static ELEMENT_METADATA: Lazy<gst::subclass::ElementMetadata> = Lazy::new(|| {
@@ -112,29 +116,292 @@ impl ElementImpl for MoqSink {
 
     fn pad_templates() -> &'static [gst::PadTemplate] {
         static PAD_TEMPLATES: Lazy<Vec<gst::PadTemplate>> = Lazy::new(|| {
-            let caps = gst::Caps::builder("video/quicktime")
-                .field("variant", "iso-fragmented")
-                .build();
-
+            // Copied from the fmp4mux source
             let pad_template = gst::PadTemplate::new(
-                "sink",
+                "sink_%u",
                 gst::PadDirection::Sink,
-                gst::PadPresence::Always,
-                &caps,
+                gst::PadPresence::Request,
+                &[
+                    gst::Structure::builder("video/x-h264")
+                        .field("stream-format", "avc")
+                        .field("alignment", "au")
+                        .build(),
+                    /*
+                    gst::Structure::builder("video/x-h265")
+                        .field("stream-format", gst::List::new(["hvc1", "hev1"]))
+                        .field("alignment", "au")
+                        .field("parsed", true)
+                        .build(),
+                        */
+                    /* TODO
+                    gst::Structure::builder("video/x-vp8")
+                        .field("width", gst::IntRange::new(1, u16::MAX as i32))
+                        .field("height", gst::IntRange::new(1, u16::MAX as i32))
+                        .build(),
+                    gst::Structure::builder("video/x-vp9")
+                        .field("profile", gst::List::new(["0", "1", "2", "3"]))
+                        .field("chroma-format", gst::List::new(["4:2:0", "4:2:2", "4:4:4"]))
+                        .field("bit-depth-luma", gst::List::new([8u32, 10u32, 12u32]))
+                        .field("bit-depth-chroma", gst::List::new([8u32, 10u32, 12u32]))
+                        .field("width", gst::IntRange::new(1, u16::MAX as i32))
+                        .field("height", gst::IntRange::new(1, u16::MAX as i32))
+                        .build(),
+                    */
+                    /*
+                    gst::Structure::builder("video/x-av1")
+                        .field("stream-format", "obu-stream")
+                        .field("alignment", "tu")
+                        .field("profile", gst::List::new(["main", "high", "professional"]))
+                        .field(
+                            "chroma-format",
+                            gst::List::new(["4:0:0", "4:2:0", "4:2:2", "4:4:4"]),
+                        )
+                        .field("bit-depth-luma", gst::List::new([8u32, 10u32, 12u32]))
+                        .field("bit-depth-chroma", gst::List::new([8u32, 10u32, 12u32]))
+                        .field("width", gst::IntRange::new(1, u16::MAX as i32))
+                        .field("height", gst::IntRange::new(1, u16::MAX as i32))
+                        .build(),
+                        */
+                    /* TODO
+                    gst::Structure::builder("audio/mpeg")
+                        .field("mpegversion", 4i32)
+                        .field("stream-format", "raw")
+                        .field("channels", gst::IntRange::new(1, u16::MAX as i32))
+                        .field("rate", gst::IntRange::new(1, i32::MAX))
+                        .build(),
+                    */
+                    /*
+                    gst::Structure::builder("audio/x-opus")
+                        .field("channel-mapping-family", gst::IntRange::new(0i32, 255))
+                        .field("channels", gst::IntRange::new(1i32, 8))
+                        .field("rate", gst::IntRange::new(1, i32::MAX))
+                        .build(),
+                        */
+                    /* TODO
+                    gst::Structure::builder("audio/x-flac")
+                        .field("framed", true)
+                        .field("channels", gst::IntRange::<i32>::new(1, 8))
+                        .field("rate", gst::IntRange::<i32>::new(1, 10 * u16::MAX as i32))
+                        .build(),
+                    */
+                ]
+                .into_iter()
+                .collect::<gst::Caps>(),
             )
             .unwrap();
 
             vec![pad_template]
         });
+
         PAD_TEMPLATES.as_ref()
+    }
+
+    fn request_new_pad(
+        &self,
+        templ: &gst::PadTemplate,
+        name: Option<&str>,
+        caps: Option<&gst::Caps>,
+    ) -> Option<gst::Pad> {
+        let name = name.unwrap_or("sink_%u");
+        let mut state = self.state.lock().unwrap();
+
+        // Check if caps is None and handle it gracefully
+        if caps.is_none() {
+            println!("request_new_pad: Caps is None, creating pad with default or template caps.");
+            let pad = gst::PadBuilder::from_template(templ).name(name).build();
+            self.obj().add_pad(&pad).unwrap();
+
+            // Configure additional pad settings if needed before returning
+            return Some(pad);
+        }
+
+        println!("request_new_pad: {:?} {} {:?}", templ, name, caps);
+        let intersected_caps = templ
+            .caps()
+            .intersect(caps.unwrap_or(&gst::Caps::new_empty()));
+        println!("intersected_caps: {:?}", intersected_caps);
+
+        let s = caps?.structure(0).unwrap();
+        let track = match s.name().as_str() {
+            "video/x-h264" => {
+                let description = s
+                    .get::<&gst::BufferRef>("codec_data")
+                    .expect("no codec_data")
+                    .map_readable()
+                    .expect("failed to map codec_data");
+
+                let constraints = s.get::<u8>("constraints").unwrap_or(0);
+                let profile = match s.get::<&str>("profile").expect("no profile") {
+                    "baseline" => 66,
+                    "main" => 77,
+                    "high" => 100,
+                    _ => panic!("unknown profile"),
+                };
+
+                // wtf is this
+                let level = s.get::<&str>("level").expect("no level");
+                let level = match level {
+                    "1" => 10,
+                    "1b" => 9,
+                    "1.1" => 11,
+                    "1.2" => 12,
+                    "1.3" => 13,
+                    "2" => 20,
+                    "2.1" => 21,
+                    "2.2" => 22,
+                    "3" => 30,
+                    "3.1" => 31,
+                    "3.2" => 32,
+                    "4" => 40,
+                    "4.1" => 41,
+                    "4.2" => 42,
+                    "5" => 50,
+                    "5.1" => 51,
+                    "5.2" => 52,
+                    _ => panic!("unknown level"),
+                };
+
+                let video = catalog::Video {
+                    track: catalog::Track {
+                        name: name.to_string(),
+                        priority: 1,
+                    },
+                    codec: catalog::H264 {
+                        constraints,
+                        profile,
+                        level,
+                    }
+                    .into(),
+                    resolution: catalog::Dimensions {
+                        width: s.get::<u32>("width").unwrap() as _,
+                        height: s.get::<u32>("height").unwrap() as _,
+                    },
+                    bitrate: s.get::<u32>("bitrate").ok(),
+                    description: Bytes::copy_from_slice(&description),
+                };
+
+                state
+                    .broadcast
+                    .as_mut()
+                    .unwrap()
+                    .create_video(video)
+                    .unwrap()
+            }
+            "audio/x-opus" => {
+                let audio = catalog::Audio {
+                    track: catalog::Track {
+                        name: name.to_string(),
+                        priority: 2,
+                    },
+                    codec: catalog::AudioCodec::Opus,
+                    bitrate: s.get::<u32>("bitrate").ok(),
+                    sample_rate: s.get::<u32>("rate").unwrap() as _,
+                    channel_count: s.get::<u32>("channels").unwrap() as _,
+                };
+
+                state
+                    .broadcast
+                    .as_mut()
+                    .unwrap()
+                    .create_audio(audio)
+                    .unwrap()
+            }
+            _ => {
+                panic!("unsupported media type: {}", s.name());
+            }
+        };
+
+        state.tracks.insert(name.to_string(), track);
+
+        let pad = Pad::builder_from_template(templ)
+            .name(name)
+            .event_function(|pad, parent, event| {
+                parent
+                    .unwrap()
+                    .downcast_ref::<super::MoqSink>()
+                    .expect("Parent object is not set")
+                    .imp()
+                    .handle_event(pad, event)
+            })
+            .chain_function(|pad, parent, buffer| {
+                parent
+                    .unwrap()
+                    .downcast_ref::<super::MoqSink>()
+                    .unwrap()
+                    .imp()
+                    .process_buffer(pad, buffer)
+            })
+            .build();
+
+        // Add the pad to the element
+        self.obj().add_pad(&pad).unwrap();
+
+        Some(pad)
+    }
+
+    fn change_state(
+        &self,
+        transition: gst::StateChange,
+    ) -> Result<gst::StateChangeSuccess, gst::StateChangeError> {
+        match transition {
+            gst::StateChange::NullToReady => {
+                let _guard = RUNTIME.enter();
+                self.setup().expect("failed to setup");
+            }
+            _ => {}
+        }
+        self.parent_change_state(transition)
     }
 }
 
+impl MoqSink {
+    fn process_buffer(
+        &self,
+        pad: &gst::Pad,
+        buffer: gst::Buffer,
+    ) -> Result<gst::FlowSuccess, gst::FlowError> {
+        let _guard = RUNTIME.enter();
+
+        let mut state = self.state.lock().unwrap();
+
+        let payload = buffer.map_readable().map_err(|_| gst::FlowError::Error)?;
+        let payload = Bytes::copy_from_slice(payload.as_slice());
+
+        let timestamp = buffer.pts().unwrap_or(buffer.dts().unwrap());
+        let timestamp = moq_karp::media::Timestamp::from_micros(timestamp.useconds());
+
+        let keyframe = !buffer.flags().contains(gst::BufferFlags::DELTA_UNIT);
+
+        let frame = moq_karp::media::Frame {
+            timestamp,
+            keyframe,
+            payload,
+        };
+
+        let track = state
+            .tracks
+            .get_mut(pad.name().as_str())
+            .expect("unknown pad");
+
+        track.write(frame);
+
+        Ok(gst::FlowSuccess::Ok)
+    }
+
+    fn handle_event(&self, _pad: &gst::Pad, event: gst::Event) -> bool {
+        match event.view() {
+            gst::EventView::Eos(_) => {
+                // Handle EOS if necessary
+                true
+            }
+            _ => true,
+        }
+    }
+}
+
+/*
 impl BaseSinkImpl for MoqSink {
     fn start(&self) -> Result<(), gst::ErrorMessage> {
-        let _guard = RUNTIME.enter();
-        self.setup()
-            .map_err(|e| gst::error_msg!(gst::ResourceError::Failed, ["Failed to connect: {}", e]))
     }
 
     fn stop(&self) -> Result<(), gst::ErrorMessage> {
@@ -163,17 +430,19 @@ impl BaseSinkImpl for MoqSink {
         Ok(gst::FlowSuccess::Ok)
     }
 }
+*/
 
 impl MoqSink {
     fn setup(&self) -> anyhow::Result<()> {
+        let mut state = self.state.lock().unwrap();
         let settings = self.settings.lock().unwrap();
-        let path = moq_transfork::Path::new(settings.path.clone());
-
-        let broadcast = moq_karp::produce::Resumable::new(path).broadcast();
-        let media = moq_karp::cmaf::Import::new(broadcast);
 
         let url = settings.url.clone().context("missing url")?;
         let url = url.parse().context("invalid URL")?;
+        let path = moq_transfork::Path::new(settings.path.clone());
+
+        let broadcast = moq_karp::produce::Resumable::new(path).broadcast();
+        state.broadcast = Some(broadcast);
 
         // TODO support TLS certs and other options
         let config = quic::Args {
@@ -186,20 +455,24 @@ impl MoqSink {
         .load()?;
 
         let client = quic::Endpoint::new(config)?.client;
-
-        let mut state = self.state.lock().unwrap();
-        state.media = Some(media);
-
         let state = self.state.clone();
 
         // We have to perform the connect in a background task because we can't block the main thread
         tokio::spawn(async move {
             let session = client.connect(&url).await.expect("failed to connect");
-            let session = moq_transfork::Session::connect(session)
+            let mut session = moq_transfork::Session::connect(session)
                 .await
                 .expect("failed to connect");
 
-            state.lock().unwrap().session = Some(session);
+            let mut state = state.lock().unwrap();
+            state
+                .broadcast
+                .as_mut()
+                .unwrap()
+                .publish(&mut session)
+                .expect("failed to publish");
+
+            state.session = Some(session);
 
             // TODO figure out how to close gstreamer gracefully on session close
         });
